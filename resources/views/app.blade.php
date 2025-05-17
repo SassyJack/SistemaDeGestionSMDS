eb44537222a515de1cf014d0145bd33b70316de2
Nicolas <ncmartinez018@gmail.com>
2025-05-17 13:54:48 -0500

    Funcionalidad Sectores con su CRUD

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,6 +11,7 @@
     <div class="container mx-auto p-4">
         <h1 class="text-2xl font-bold mb-4">Menú de Gestión</h1>
         @include('partials.menu')
+        @yield('content')
     </div>
 </body>
 </html>
