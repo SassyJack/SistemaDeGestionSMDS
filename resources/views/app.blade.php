2c84daf6b4f2eba88cec8765ecc9ac909ea1cc02
Johan <johancortes819@gmail.com>
2025-05-20 16:39:15 -0500

    Prueba fallo en menu

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Gestión</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+ 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
         #sidebar {
